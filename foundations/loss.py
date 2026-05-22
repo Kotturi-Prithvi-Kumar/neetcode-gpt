b30f52a6a1a4ec9d3a4bc2a6efa042a86189dfa2
Kotturi Prithvi <kotturiprithvi8005@gmail.com>
2026-05-22 18:06:15 +0530

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -4,8 +4,14 @@ from numpy.typing import NDArray
 
 class Solution:
 
-    def softmax(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
-        
-        shift = z - np.max(z)
-        exp = np.exp(shift)
-        return np.round(exp / np.sum(exp),4)
+    def binary_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        epsilon = 1e-7
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        loss = -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+        return round(loss, 4)
+
+    def categorical_cross_entropy(self, y_true: NDArray[np.float64], y_pred: NDArray[np.float64]) -> float:
+        epsilon = 1e-7
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        loss = -np.mean(np.sum(y_true * np.log(y_pred), axis=1))
+        return round(loss, 4)
